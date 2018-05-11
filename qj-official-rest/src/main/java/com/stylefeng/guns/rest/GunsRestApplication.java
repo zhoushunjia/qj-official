8d905bb23d0d71476caf15b903cbe051079036a8
zhoushunjia <947723090@qq.com>
2018-05-11 14:11:50 +0800

    更新

--- qj-official-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
+++ qj-official-rest/src/main/java/com/stylefeng/guns/rest/GunsRestApplication.java
@@ -3,7 +3,7 @@ package com.stylefeng.guns.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.stylefeng.guns"})
+@SpringBootApplication(scanBasePackages = {"com.stylefeng.guns","com.qj.official"})
 public class GunsRestApplication {
 
     public static void main(String[] args) {
